5eb09dca133a81a3f6940471cf06d37222644b72
AKochiev <akochiev@mtsbank.ru>
2021-08-20 17:50:48 +0300

    asd

--- src/trigger.js
+++
@@ -1,9 +0,0 @@
-export const trigger = (str, el1, el2) => {
-  if (str === "el1") {
-    el2.classList.add("unvisible");
-    el1.classList.remove("unvisible");
-  } else {
-    el1.classList.add("unvisible");
-    el2.classList.remove("unvisible");
-  }
-};
